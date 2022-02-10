7703b1b0a47fc3f49185bab7249faab2d9a7900f
Fergus <intel4004u@gmail.com>
2022-02-10 18:32:20 +0000

    412. INTERMEDIATE Campground New & Create
    To create a new record (campground) we need two different routes:
    	The 'new form' route for entering data and the 'create route' for posting data i.e. a post.
    	Create 'new.ejs' for the form so users can enter 'title' and 'location' (don't worry about cosmetics)
    	Create the 'new form' route in 'app.js'
    	You may come up against a perplexing problem if 'new' form won't display. Clue is 'route order'
    	Now setup your post route in 'app.js' then simply render the your form data for now:
    		If this doesn't display? Clue: parsing

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ db.once('open', () => {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   // res.send('Lo');
@@ -27,6 +28,14 @@ app.get('/campgrounds', async (req, res) => {
   const campgrounds = await CampGround.find({});
   res.render('campgrounds/index', { campgrounds });
 });
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+  res.send(req.body)
+})
 
 app.get('/campgrounds/:id', async (req, res) => {
   const campGround = await CampGround.findById(req.params.id)
