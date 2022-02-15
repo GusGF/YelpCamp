37bcc00ba17e742b05c40afd0d7ac2c214ce106f
Fergus <intel4004u@gmail.com>
2022-02-15 17:55:31 +0000

    414. Campground Delete
    Create a delete route:
    	Get the ID from the params
    	Find it and delete it
    	Redirect to the index template
    Now make the button to send that delete request:
    	In the template 'show.ejs':
    		Use a form element and a button to call the 'delete' route to action the delete

--- app.js
+++ app.js
@@ -61,6 +61,12 @@ app.put('/campgrounds/:id', async (req, res) => {
   res.redirect(`/campgrounds/${campground._id}`)
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await CampGround.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
+})
+
 app.listen(3001, () => {
   console.log('Serving on port 3000')
 });
