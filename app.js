8bff7654309685cb1d144ccdd561a01d1477cf46
Fergus <intel4004u@gmail.com>
2022-02-15 16:45:19 +0000

    413. Campground Edit & Update
    Add a route for when a campground is to be edited:
    	i.e. '/campgrounds/:id/edit'
    	And display the campground in a 'edit' template called 'edit.ejs'
    		This template should display the campsite obviously
    	In the show template add an edit link for the current displayed campground and test it
    		Make sure the link takes you to the edit template now displaying the campground to be edited
    	Add a button to update the campground in the 'edit' template
    	Install & setup a package which allows us to override our get/post form requests in HTML
    	Write a simple put route:
    		In the 'edit' template override the form's method
    		Write a bare bones 'put' route and output a simple console message
    		Now setup the action for the 'edit' template's form
    		In 'app.js' develop the put route:
    			Use the campgrounds ID to find and update the document
    				Try using the spread operator to assign the values
    				Redirect to the display the result of the edit using the campground template 'show.ejs'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const CampGround = require('./models/campground');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/yelpCampDB', {
   useNewUrlParser: true,
@@ -18,28 +19,46 @@ db.once('open', () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-  // res.send('Lo');
   res.render('home');
 })
 
+// Full list
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await CampGround.find({});
   res.render('campgrounds/index', { campgrounds });
 });
 
+// Form for adding a new campground
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 })
 
+// Adding a campground
 app.post('/campgrounds', async (req, res) => {
-  res.send(req.body)
+  const campground = new CampGround(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 })
 
+// Display a campground
 app.get('/campgrounds/:id', async (req, res) => {
   const campGround = await CampGround.findById(req.params.id)
   res.render('campgrounds/show', { campGround });
+})
+
+// Display a campground to edit
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campGround = await CampGround.findById(req.params.id)
+  res.render('campgrounds/edit', { campGround });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await CampGround.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.listen(3001, () => {
