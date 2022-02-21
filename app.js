2035bf42904210405cfc6b01daec2d53e775aeb9
Fergus <intel4004u@gmail.com>
2022-02-21 10:26:48 +0000

    423. A New EJS Tool For Layouts (Injecting layouts)
    This is sort of similar to how template literals work
    Install node package ejs-mate
    Tell express that we are using an engine for EJS
    In your 'views' folder define a new folder called 'layouts'
    In layouts create a boilerplate file so only the content of the body is unique
    Apply to edit, new, show, index, campground

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const CampGround = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelpCampDB', {
   useNewUrlParser: true,
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 // http://localhost:3001/campgrounds?password=pass
 const verify = ((req, res, next) => {
