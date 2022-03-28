91c174ac2fa9dacba14f6db8696d85be4ca7c239
Fergus <intel4004u@gmail.com>
2022-03-28 17:17:16 +0100

    447. More Errors
    Add 'app.all' for picking up bad routes:
    	Test it
    Now use our 'ExpressError' class in 'app.all' so it hits our default custom error handler:
    	In the custom error handler destructure the error passed in and assign defaults
    	Test it
    Test a bad route
    Now test a add new campground form with bad data, what happend?
    Okay then to remedy this:
    	Throw an 'ExpressError' if the 'campground' object coming back is empty supplying status & msg:
    		Check what comes back in postman

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const AppError = require('./other/AppError');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/yelpCampDB', {
   useNewUrlParser: true,
@@ -51,33 +52,24 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
-// Form for adding a new campground
+// Display form for adding a new campground
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 })
 
 // Add a campground
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+  if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
   const campground = new CampGround(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-// // Display a campground
-// app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-//   const campGround = await CampGround.findById(req.params.id)
-//   res.render('campgrounds/show', { campGround });
-// }))
-
 // Display a campground
-app.get('/campgrounds/:id', async (req, res, next) => {
-  try {
-    const campGround = await CampGround.findById(req.params.id)
-    res.render('campgrounds/show', { campGround });
-  } catch (e) {
-    next();
-  }
-})
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
+  const campGround = await CampGround.findById(req.params.id)
+  res.render('campgrounds/show', { campGround });
+}))
 
 // Display a campground to edit
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
@@ -100,11 +92,18 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   res.redirect('/campgrounds');
 }));
 
+// Will only run if nothing else above matches
+// the * represents a 'get', a 'post', etc. 
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page not found', 404));
+})
+
 app.use((err, req, res, next) => {
-  console.log('*******************************************');
-  console.log('*************** ERROR ********************');
-  console.log('*******************************************');
-  res.send("Oops not good");
+  console.log('**************************************');
+  console.log('*************** ERROR ****************');
+  console.log('**************************************');
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).send(message);
 })
 
 app.listen(3001, () => {
