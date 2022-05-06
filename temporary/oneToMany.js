f383f50d47c4f87c35578dcdf407365703f0b2de
Fergus <intel4004u@gmail.com>
2022-05-06 18:10:41 +0100

    456. Mongoose Populate
    Here we're looking at populating the Product object IDs associated with a Farm
    So lets start by accessing the data for a farm without accessing all the product stuff i.e. just the object IDs
    	Post this to the console
    Now lets do the same but seeing all the product related stuff for each object ID
    	Again post to the console

--- temporary/oneToMany.js
+++ temporary/oneToMany.js
@@ -30,16 +30,29 @@ Product.insertMany([
 const farmSchema = new mongoose.Schema({
   name: String,
   city: String,
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
-  // Here we tell Mongoose this type i.e. it is an object ID type. It's not JS it's mongoose
-  // The ref option is what tells Mongoose which model to use during population i.e. an array of 'Product' IDs.
+  products: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product'
+  }]
+  // And this is where the 'ref' comes in that we set up, this ref product. This is our way of telling 
+  // Mongoose this is an array of object IDs and each one represents or is connected to an actual product model.
 })
 const Farm = mongoose.model('Farm', farmSchema);
+// Here we have a function to make a farm after which we push a product to it
 const makeFarm = async () => {
   const farm = new Farm({ name: 'Full Belly Farms', city: 'Guinda, CA' });
-  const melon = await Product.findOne({ name: 'Goddess Melon' });
-  farm.products.push(melon);
+  const melon1 = await Product.findOne({ name: 'Goddess Melon' });
+  farm.products.push(melon1);
+  const melon2 = await Product.findOne({ name: 'Sugar Baby Watermelon' });
+  farm.products.push(melon2);
+  const melon3 = await Product.findOne({ name: 'Asparagus' });
+  farm.products.push(melon3);
   await farm.save();
   console.log(farm);
 }
 makeFarm();
+Farm.findOne({ name: 'Full Belly Farms' }).then(product => console.log(product));
+// Each one of our object IDs represents or is connected to an actual product model. So when we call populate, 
+// Mongoose is going to take each I.D. that has been stored in the 'Product' array and replace with the 
+// corresponding product, but we have to make sure we tell it to populate.
+Farm.findOne({ name: 'Full Belly Farms' }).populate('products').then(theFarm => console.log(theFarm));
