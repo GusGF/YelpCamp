b457561ba6413faa84bbb2869443279dc172ef22
Fergus <intel4004u@gmail.com>
2022-05-18 16:28:12 +0100

    466. Deletion Mongoose Middleware
    Setup a delete route '/farms/:id'
    Use a delete link on the farm's show page under 'add product' & 'all farms' links
    When a farm is deleted all associated products should also be deleted using mongoose middleware:
    	Create mongoose middleware which will fire before and after a farm deletion:
    		Set the pre & post middleware to output to the console what they are deleting
    	Now setup post-delete middleware that picks out the product IDs and deletes them:
    		Print the products array to the console
    		Using the Mongo operatior '$in' delete all products whose ID is in 'farm.products' array
    		Print out the result to the delete operation

--- temporary/models/farm.js
+++ temporary/models/farm.js
@@ -3,32 +3,48 @@ const Product = require('./product');
 const { Schema } = mongoose;
 
 const farmSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Farm must have a name!']
-    },
-    city: {
-        type: String
-    },
-    email: {
-        type: String,
-        required: [true, 'Email required']
-    },
-    products: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Product'
-        }
-    ]
+  name: {
+    type: String,
+    required: [true, 'Farm must have a name!']
+  },
+  city: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: [true, 'Email required']
+  },
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    }
+  ]
 });
 
+
+// Below we create Mongoose middleware to perform a delete when a farm is deleted
 // DELETE ALL ASSOCIATED PRODUCTS AFTER A FARM IS DELETED
 farmSchema.post('findOneAndDelete', async function (farm) {
-    if (farm.products.length) {
-        const res = await Product.deleteMany({ _id: { $in: farm.products } })
-        console.log(res);
-    }
+  console.log(`Products are: ${farm.products}`);
+  if (farm.products.length) {
+    // Using the Mongo operatior '$in' we can delete all products whose ID is in 'farm.products' array
+    const res = await Product.deleteMany({ _id: { $in: farm.products } })
+    console.log(res);
+  }
 })
+
+// // In the Pre-middleware we don't have access to the farm data as it's running before the 
+// // delete query unlike with the Post-middleware as it runs after the farm is fetched and
+// // deleted. In the post we can pull off the associated products and delete them.
+// farmSchema.pre('findOneAndDelete', async function (data) {
+//   console.log("PRE MIDDLEWARE!!!");
+//   console.log(data);
+// })
+// farmSchema.post('findOneAndDelete', async function (data) {
+//   console.log("POST MIDDLEWARE!!!");
+//   console.log(data);
+// })
 
 const Farm = mongoose.model('Farm', farmSchema);
 
