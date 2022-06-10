9a9718c9fe37ec13e2823e0351d7ab587182b368
Fergus <intel4004u@gmail.com>
2022-06-10 17:49:08 +0100

    480. Cookie Parser Middleware
    The browser stores the cookies and they are associated with the URL requested
    Use node's cookie parser and console log the stored cookies for the URL concerned
    Add a cookie via the Chrome DevTools interface

--- temporaryTwo/index.js
+++ temporaryTwo/index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+app.use(cookieParser('thisIsMySecret'));
+
 
 app.get('/greet', (req, res) => {
-  res.send("Hey there!")
+  // console.log(req.cookies);
+  res.send("Press F12 and see all the cookies for this domain")
 })
 
-// These will be sent on every request
-app.get('/setname', (req, res) => {
-  res.cookie('name', 'Stevie-Chicks');
-  res.cookie('animal', 'Kitty');
-  res.send('Okay sent you a cookie');
+// Cookies are key value pairs
+app.get('/sendCookies', (req, res) => {
+  // sending cookie to the requesting client
+  res.cookie('My First Cookie', 'Bruce Springsteen');
+  res.send('Sent a cookie');
+})
+
+app.get('/sendSignedCookies', (req, res) => {
+  // sending signed cookie to the requesting client
+  res.cookie('My Signed Cookie', 'banana', { signed: true });
+  res.send('Sent a signed cookie');
+})
+
+// The browser will store the cookies above, in the client's browser data 
+// and will do so for this site only i.e. http://localhost
+// Then these cookies will be sent back to the server in every request the 
+// client makes hence we can view them by accessing the 'req' parameter
+app.get('/verifyCookies', (req, res) => {
+  res.send(req.cookies);
+})
+
+// The cookies sent are encrypted, tampering with them will remove the cookie completely
+// in the background will no longer be sent with the req object.
+app.get('/verifysignedCookies', (req, res) => {
+  res.send(req.signedCookies);
 })
 
 app.listen(4000, () => {
