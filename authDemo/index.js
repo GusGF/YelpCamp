757ad000cebc349c339371ac35fbbff3e7a1a46e
Fergus <intel4004u@gmail.com>
2022-11-07 17:43:30 +0000

    503. Auth Demo:Registering

--- authDemo/index.js
+++ authDemo/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
 const User = require('./models/user')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+mongoose.connect('mongodb://127.0.0.1:27017/authDemo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Mongo open')
+}).catch(err => {
+  console.log('Mongo error')
+  console.log(err)
+})
+
+// This parses the request body so express knows what to expect
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
+
+app.get('/', (req, res) => {
+  res.send('This is the home page')
+})
 
 app.get('/secret', (req, res) => {
   res.send('This is secret')
@@ -11,6 +30,17 @@ app.get('/secret', (req, res) => {
 
 app.get('/register', (req, res) => {
   res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+  const { password, username } = req.body;
+  const hash = await bcrypt.hash(password, 12)
+  const user = User({
+    username: username,
+    password: hash
+  })
+  await user.save()
+  res.redirect('/')
 })
 
 app.listen(3001, () => {
